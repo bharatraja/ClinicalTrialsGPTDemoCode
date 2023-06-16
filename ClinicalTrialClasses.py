fe3d6bc7017fb012a33076f53931fb34c67437b9
Bharat Rajagopal <bharatraja@hotmail.com>
2023-06-16 12:21:06 -0500

    Refactored

--- ClinicalTrialClasses.py
+++ ClinicalTrialClasses.py
@@ -1,6 +1,265 @@
+#Provides all supporting classes for ClinicalTrials.py
 
-class TestClass:
-    name=""
+
+import streamlit as st
+import requests
+from requests.exceptions import HTTPError
+import json
+import urllib.parse
+import os
+from geopy.geocoders import Nominatim
+import pandas as pd
+
+#region TODOS
+
+#endregion
+
+DEBUG=True
+
+@st.cache_data
+def getQueryResultsFromCTGov(query=""):
+    return requests.get(query)
+
+
+@st.cache_data
+def findGeocode(city):
+    # try and catch is used to overcome
+    # the exception thrown by geolocator
+    # using geocodertimedout  
+    try:
+          
+        # Specify the user_agent as your
+        # app name it should not be none
+        geolocator = Nominatim(user_agent="BharatTestApp")
+        return geolocator.geocode(city)
+      
+    except:
+        return None  
+
+
+
+def camel_case_split(str):
+    #first word will 
+    words = [[str[0].upper()]]
+
+ 
+    for c in str[1:]:
+        if words[-1][-1].islower() and c.isupper():
+            words.append(list(c))
+        else:
+            words[-1].append(c)
+ 
+    words= [''.join(word) for word in words]
+    return ' '.join(words)
+
+#region CTCode 
+
+#region TrialQuery
+class TrialsQuery:
+    condition="" #condition
+    treatment="" #treament same as intervention
+    location=""  #location could be a city but will expand later
+    range="100mi"    #default range
+    max_records=50
+    lat=""
+    long=""
+    studyStatus=""
+    other=""
+
+    #region api endpoints
+    api_base="https://beta.clinicaltrials.gov/api/v2"
+    api_key="" 
+    api_studies="/studies"
+    api_studies_detail="/studies/"
+    api_studies_metadata="/studies/metadata"
+    api_stats="/stats/size"
+    api_field_values="/stats/fieldValues"
+    api_field_detail="/stats/fieldValues/"
+    api_list_size="/stats/listSizes"
+    api_list_field_size="/stats/listSizes/"
+    api_version="/version"
+    #endregion
+
+    def __init__(self, condition="", treatment="", location="", studyStatus=[], other=""):
+        self.condition=condition
+        self.treatment=treatment
+        self.studyStatus=",".join(studyStatus)
+
+        self.location=location
+        self.other=other
+        if location != "":
+            gc=findGeocode(location)
+            try:
+                self.lat=str(gc.latitude)
+                self.long=str(gc.longitude)
+            except:
+                #st.write("Error in Location")
+                return
+           
+
+    def getStudiesQuery(self, fields=None)->str:
+
+        #need to urlencode
+        if fields is None:
+            fields="NCTId,BriefTitle,LeadSponsorName,LocationCity,LocationFacility,InterventionName,PrimaryOutcomeMeasure,BriefSummary" #cannot have spaces
+
+        query=( self.api_base + self.api_studies + "?" +
+              f"query.cond={self.condition}&" +
+              f"query.intr={self.treatment}&" +
+              f"query.term={self.other}&" +
+              f"fields={fields}&" +
+              "countTotal=true&" +
+              f"pageSize={self.max_records}&"
+          )
+        if self.lat != "":
+            query=query + f"postFilter.geo=distance({self.lat},{self.long},{self.range})&" + f"query.locn={self.location}&"
+
+        if self.studyStatus != "":
+            query+= f"postFilter.overallStatus={self.studyStatus}&"
+
+        #if DEBUG:
+        #    st.write(urllib.parse.quote_plus(query,'/:&?='))
+
+        
+        return urllib.parse.quote_plus(query,'/:&?=(),')
+
+    def __str__(self) -> str:
+        return f"{{ \
+            condition: {self.condition}, \
+            treatment: {self.treatment}, \
+            location: {self.location}, \
+        }}"
     
-    def __init__(self, name="") -> None:
-           self.name=name 
+#endregion
+
+#region Study
+class Study:
+   raw=""
+   nctid=""
+   leadSponsor=""
+   briefTitle=""
+   interventionName=""
+   primaryOutcomeMeasure=""
+   briefSummary=""
+   locationCity=""
+   locationFacility=""
+
+
+   def collate(self, arr=[],key=""):
+       try: 
+        intr=[t[key] for t in arr]
+        return ', '.join(intr)
+       except:
+           return ""
+
+   
+   def getValueIfExists(self, keysArr=[],raw=""):
+       if len(keysArr) >1 and len(raw) !=0:
+           #if raw[keysArr[0]]:
+           if keysArr[0] in raw:
+               popped=keysArr.pop(0)
+               #st.write(popped)
+               #st.write(keysArr)
+               return self.getValueIfExists(keysArr,raw[popped])
+           else:
+               return ""
+       else:
+           #st.write(raw)
+           if len(raw) != 0:
+               return raw[keysArr[0]]
+           else:
+               return ""
+        
+
+   def __init__(self,raw=""):
+       self.raw=raw
+
+       #process raw
+       if raw != "":
+           try:
+               self.nctid=raw['protocolSection']['identificationModule']['nctId']
+               self.briefTitle=raw['protocolSection']['identificationModule']['briefTitle']
+               self.leadSponsor=raw['protocolSection']['sponsorCollaboratorsModule']['leadSponsor']['name']
+               self.briefSummary=raw['protocolSection']['descriptionModule']['briefSummary']
+               #if 'armsInterventionsModule' in raw['protocolSection']:
+               # if 'interventions' in raw['protocolSection']['armsInterventionsModule']:
+               #    self.interventionName=self.collate(raw['protocolSection']['armsInterventionsModule']['interventions'],"name")
+               
+               self.interventionName=self.collate( self.getValueIfExists(['armsInterventionsModule', 'interventions'],
+                                                 raw['protocolSection']), 'name')
+               
+               self.locationFacility=self.collate( self.getValueIfExists(['contactsLocationsModule', 'locations'], 
+                                                 raw['protocolSection']), 'facility')
+
+               self.locationCity=self.collate(self.getValueIfExists(['contactsLocationsModule','locations'], 
+                                                                    raw['protocolSection']), 'city')
+               self.primaryOutcomeMeasure=self.collate(self.getValueIfExists(['outcomesModule', 'primaryOutcomes'],
+                                                                   raw['protocolSection']), "measure")  
+               
+           except:
+               st.write(f"Error in study data {self.nctid}")
+               #st.write(raw)
+
+
+           
+
+#endregion
+
+#region Trials Class
+class Trials:
+  query:TrialsQuery
+  response=None
+  raw_json=None
+  totalCount=0
+  studies=[]
+
+  def __init__(self,query:TrialsQuery=None):
+      self.query=query
+      self.studies=[]
+
+  def getStudies(self, query:TrialsQuery=None)->None:
+      if query != None:
+          self.query=query
+      
+      try:
+          url=self.query.getStudiesQuery()
+          r=getQueryResultsFromCTGov(url)
+          self.response=r
+
+          self.raw_json=j=r.json()
+          #st.write(j)
+          self.totalCount=j['totalCount']
+
+          #studies for now studies are just nct
+          self.studies=list(map (lambda x:  Study(x), j['studies']))
+          #st.write(f"Total Number of studies:{self.totalCount}")
+          #st.write(f"No of Studies pulled={len(self.studies)}")
+          return self.response
+      
+      except HTTPError as err:
+          st.write(f"HTTP error occured in Trials.getStudies(): {err}")
+      except:
+          st.write("Error")
+          return None
+      return None #
+  
+  #Returns studies as a datafram
+  def getStudiesAsDF(self):
+    if self.totalCount!=0:
+        df = pd.DataFrame([t.__dict__ for t in self.studies ])
+        df.drop(['raw'],axis=1,inplace=True)
+
+        #write meaningful column names
+        df.columns=[camel_case_split(str(n)) for n in df.columns]
+        
+        
+
+        return df
+    else:
+        return None
+   
+      
+
+#endregion
+
+#endregion
--- ClinicalTrials.py
+++ ClinicalTrials.py
@@ -11,33 +11,25 @@ import pandas as pd
 from langchain.chat_models import AzureChatOpenAI
 from langchain.schema import HumanMessage
 from langchain.agents import create_pandas_dataframe_agent
-from tenacity import retry, wait_random_exponential, stop_after_attempt  
+from tenacity import retry, wait_random_exponential, stop_after_attempt 
+import ClinicalTrialClasses as CT
 
 DEBUG=True
 
 st.set_page_config(page_title="Clinical Trials Companion",  page_icon=":robot_face:", layout="wide")
+hide_streamlit_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
 
 #region TODOS
  #ICON next to Title
  #Do some optimization so we are not querying all the time
  #remove @st.cache_data from findGeocode
 #endregion
-
-def camel_case_split(str):
-    #first word will 
-    words = [[str[0].upper()]]
-
- 
-    for c in str[1:]:
-        if words[-1][-1].islower() and c.isupper():
-            words.append(list(c))
-        else:
-            words[-1].append(c)
- 
-    words= [''.join(word) for word in words]
-    return ' '.join(words)
-
-
 
 @st.cache_resource
 def getChatModel():
@@ -50,239 +42,6 @@ def getChatModel():
     )
     return model
     
-
-@st.cache_data
-def getQueryResultsFromCTGov(query=""):
-    return requests.get(query)
-
-
-@st.cache_data
-def findGeocode(city):
-    # try and catch is used to overcome
-    # the exception thrown by geolocator
-    # using geocodertimedout  
-    try:
-          
-        # Specify the user_agent as your
-        # app name it should not be none
-        geolocator = Nominatim(user_agent="BharatTestApp")
-        return geolocator.geocode(city)
-      
-    except:
-        return None  
-
-#region CTCode 
-
-#region TrialQuery
-class TrialsQuery:
-    condition="" #condition
-    treatment="" #treament same as intervention
-    location=""  #location could be a city but will expand later
-    range="100mi"    #default range
-    max_records=50
-    lat=""
-    long=""
-    studyStatus=""
-    other=""
-
-    #region api endpoints
-    api_base="https://beta.clinicaltrials.gov/api/v2"
-    api_key="" 
-    api_studies="/studies"
-    api_studies_detail="/studies/"
-    api_studies_metadata="/studies/metadata"
-    api_stats="/stats/size"
-    api_field_values="/stats/fieldValues"
-    api_field_detail="/stats/fieldValues/"
-    api_list_size="/stats/listSizes"
-    api_list_field_size="/stats/listSizes/"
-    api_version="/version"
-    #endregion
-
-    def __init__(self, conditon="", treatment="", location="", studyStatus=[], other=""):
-        self.condition=condition
-        self.treatment=treatment
-        self.studyStatus=",".join(studyStatus)
-
-        self.location=location
-        self.other=other
-        if location != "":
-            gc=findGeocode(location)
-            try:
-                self.lat=str(gc.latitude)
-                self.long=str(gc.longitude)
-            except:
-                #st.write("Error in Location")
-                return
-           
-
-    def getStudiesQuery(self, fields=None)->str:
-
-        #need to urlencode
-        if fields is None:
-            fields="NCTId,BriefTitle,LeadSponsorName,LocationCity,LocationFacility,InterventionName,PrimaryOutcomeMeasure,BriefSummary" #cannot have spaces
-
-        query=( self.api_base + self.api_studies + "?" +
-              f"query.cond={self.condition}&" +
-              f"query.intr={self.treatment}&" +
-              f"query.term={self.other}&" +
-              f"fields={fields}&" +
-              "countTotal=true&" +
-              f"pageSize={self.max_records}&"
-          )
-        if self.lat != "":
-            query=query + f"postFilter.geo=distance({self.lat},{self.long},{self.range})&" + f"query.locn={self.location}&"
-
-        if self.studyStatus != "":
-            query+= f"postFilter.overallStatus={self.studyStatus}&"
-
-        #if DEBUG:
-        #    st.write(urllib.parse.quote_plus(query,'/:&?='))
-
-        
-        return urllib.parse.quote_plus(query,'/:&?=(),')
-
-    def __str__(self) -> str:
-        return f"{{ \
-            condition: {self.condition}, \
-            treatment: {self.treatment}, \
-            location: {self.location}, \
-        }}"
-    
-#endregion
-
-#region Study
-class Study:
-   raw=""
-   nctid=""
-   leadSponsor=""
-   briefTitle=""
-   interventionName=""
-   primaryOutcomeMeasure=""
-   briefSummary=""
-   locationCity=""
-   locationFacility=""
-
-
-   def collate(self, arr=[],key=""):
-       try: 
-        intr=[t[key] for t in arr]
-        return ', '.join(intr)
-       except:
-           return ""
-
-   
-   def getValueIfExists(self, keysArr=[],raw=""):
-       if len(keysArr) >1 and len(raw) !=0:
-           #if raw[keysArr[0]]:
-           if keysArr[0] in raw:
-               popped=keysArr.pop(0)
-               #st.write(popped)
-               #st.write(keysArr)
-               return self.getValueIfExists(keysArr,raw[popped])
-           else:
-               return ""
-       else:
-           #st.write(raw)
-           if len(raw) != 0:
-               return raw[keysArr[0]]
-           else:
-               return ""
-        
-
-   def __init__(self,raw=""):
-       self.raw=raw
-
-       #process raw
-       if raw != "":
-           try:
-               self.nctid=raw['protocolSection']['identificationModule']['nctId']
-               self.briefTitle=raw['protocolSection']['identificationModule']['briefTitle']
-               self.leadSponsor=raw['protocolSection']['sponsorCollaboratorsModule']['leadSponsor']['name']
-               self.briefSummary=raw['protocolSection']['descriptionModule']['briefSummary']
-               #if 'armsInterventionsModule' in raw['protocolSection']:
-               # if 'interventions' in raw['protocolSection']['armsInterventionsModule']:
-               #    self.interventionName=self.collate(raw['protocolSection']['armsInterventionsModule']['interventions'],"name")
-               
-               self.interventionName=self.collate( self.getValueIfExists(['armsInterventionsModule', 'interventions'],
-                                                 raw['protocolSection']), 'name')
-               
-               self.locationFacility=self.collate( self.getValueIfExists(['contactsLocationsModule', 'locations'], 
-                                                 raw['protocolSection']), 'facility')
-
-               self.locationCity=self.collate(self.getValueIfExists(['contactsLocationsModule','locations'], 
-                                                                    raw['protocolSection']), 'city')
-               self.primaryOutcomeMeasure=self.collate(self.getValueIfExists(['outcomesModule', 'primaryOutcomes'],
-                                                                   raw['protocolSection']), "measure")  
-               
-           except:
-               st.write(f"Error in study data {self.nctid}")
-               #st.write(raw)
-
-
-           
-
-#endregion
-
-#region Trials Class
-class Trials:
-  query:TrialsQuery
-  response=None
-  raw_json=None
-  totalCount=0
-  studies=[]
-
-  def __init__(self,query:TrialsQuery=None):
-      self.query=query
-      self.studies=[]
-
-  def getStudies(self, query:TrialsQuery=None)->None:
-      if query != None:
-          self.query=query
-      
-      try:
-          url=self.query.getStudiesQuery()
-          r=getQueryResultsFromCTGov(url)
-          self.response=r
-
-          self.raw_json=j=r.json()
-          #st.write(j)
-          self.totalCount=j['totalCount']
-
-          #studies for now studies are just nct
-          self.studies=list(map (lambda x:  Study(x), j['studies']))
-          #st.write(f"Total Number of studies:{self.totalCount}")
-          #st.write(f"No of Studies pulled={len(self.studies)}")
-          return self.response
-      
-      except HTTPError as err:
-          st.write(f"HTTP error occured in Trials.getStudies(): {err}")
-      except:
-          st.write("Error")
-          return None
-      return None #
-  
-  #Returns studies as a datafram
-  def getStudiesAsDF(self):
-    if self.totalCount!=0:
-        df = pd.DataFrame([t.__dict__ for t in self.studies ])
-        df.drop(['raw'],axis=1,inplace=True)
-
-        #write meaningful column names
-        df.columns=[camel_case_split(str(n)) for n in df.columns]
-        
-        
-
-        return df
-    else:
-        return None
-   
-      
-
-#endregion
-
-#endregion
-
 
 #region Begin Main UI Code
 #@st.cache_data breaks the way the controls function
@@ -360,6 +119,7 @@ search=st.sidebar.button("Find and Chat")
 st.title(":robot_face: Clinical Trials Demo GPT Copilot")
 
 
+
 with st.expander("", expanded=True):
     if condition or treatment or location or studyStatus or other:
         st.subheader("You current Search Criteria")
@@ -376,7 +136,7 @@ left_column,  right_column = st.columns([.5,.5])
 #if condition or treatment or location:
 #if search or condition or treatment or location:
 if search or st.session_state['refreshData']:
-    trials=Trials(TrialsQuery(condition, treatment, location, studyStatus, other))
+    trials=CT.Trials(CT.TrialsQuery(condition, treatment, location, studyStatus, other))
     trials.getStudies()
 
  
